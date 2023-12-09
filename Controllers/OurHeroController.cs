61103814579b9e8790c66192ed9ddd521b5cd224
Ankit <urankitsahu92@gmail.com>
2023-12-09 21:50:46 +0530

    Add project files.

---
+++ Controllers/OurHeroController.cs
@@ -0,0 +1,87 @@
+﻿using DotNet8WebAPI.Model;
+using DotNet8WebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8WebAPI.Controllers
+{   
+    [Route("api/[controller]")] //    /api/OurHero
+    [ApiController]
+    public class OurHeroController : ControllerBase
+    {
+        private readonly IOurHeroService _heroService;
+        public OurHeroController(IOurHeroService heroService)
+        {
+            _heroService = heroService;
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromQuery] bool? isActive = null)
+        {
+            var heros = _heroService.GetAllHeros(isActive);
+            return Ok(heros);
+        }
+
+        [HttpGet("{id}")]
+        //[Route("{id}")] // /api/OurHero/:id
+        public IActionResult Get(int id)
+        {
+            var hero = _heroService.GetHerosByID(id);
+            if (hero == null)
+            {
+                return NotFound();
+            }
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AddUpdateOurHero heroObject)
+        {
+            var hero = _heroService.AddOurHero(heroObject);
+
+            if (hero == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(new
+            {
+                message = "Super Hero Created Successfully!!!",
+                id = hero!.Id
+            });
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put([FromRoute] int id, [FromBody] AddUpdateOurHero heroObject)
+        {
+            var hero = _heroService.UpdateOurHero(id, heroObject);
+            if (hero == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new
+            {
+                message = "Super Hero Updated Successfully!!!",
+                id = hero!.Id
+            });
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            if (!_heroService.DeleteHerosByID(id))
+            {
+                return NotFound();
+            }
+
+            return Ok(new
+            {
+                message = "Super Hero Deleted Successfully!!!",
+                id = id
+            });
+        }
+    }
+}
---
+++ Services/IOurHeroService.cs
@@ -0,0 +1,17 @@
+﻿using DotNet8WebAPI.Model;
+
+namespace DotNet8WebAPI.Services
+{
+    public interface IOurHeroService
+    {
+        List<OurHero> GetAllHeros(bool? isActive);
+
+        OurHero? GetHerosByID(int id);
+
+        OurHero AddOurHero(AddUpdateOurHero obj);
+
+        OurHero? UpdateOurHero(int id, AddUpdateOurHero obj);
+
+        bool DeleteHerosByID(int id);
+    }
+}
